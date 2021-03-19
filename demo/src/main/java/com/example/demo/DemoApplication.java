cbf5838fddfb051213a7d294a2ad1b7183926f76
sandres-unizar <79507425+sandres-unizar@users.noreply.github.com>
2021-03-19 13:44:04 +0100

    cambiar cadena linea 67 Demo

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -64,7 +64,7 @@ public class DemoApplication {
 	@PostMapping("/message")
 	public ResponseEntity<Explainer> hello(@RequestParam(value = "who", defaultValue = "World") String name) throws InterruptedException {
 
-		launcher.sendMessage("Este es el mansaje esperado");
+		launcher.sendMessage("Este es el mansaje");
 		return ResponseEntity.ok(new Explainer("Enviando mensaje a " + name));
 	}
 }
